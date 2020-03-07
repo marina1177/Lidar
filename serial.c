4bf1b0380b8bb490de77c4d51e1d59891babdeb3
marina1177 <marina1177@gmail.com>
2020-03-07 15:51:22 +0300

    read data

--- serial.c
+++ serial.c
@@ -1,10 +1,30 @@
 #include <unistd.h>
 #include<stdlib.h>
 #include <stdio.h>
+
 #include<wiringPi.h>
+#include<wiringSerial.h>
 
 int main()
 {
 	printf("<code>\n");
+	int fd;
+
+	//fd = open("/dev/ttyA0", O_RDWR);
+	if((fd = serialOpen("/dev/ttyACM0",115200)) < 0)
+	{
+		printf("error_fd\n");
+		return(1);		
+	}
+	printf("serial test start...\n");
+
+	//serialPrintf(fd, "\r";
+	
+	while(serialDataAvail(fd))
+	{
+		printf("%c", serialGetchar(fd));
+	}
+	serialClose(fd);
+	return(0);
 }
 
